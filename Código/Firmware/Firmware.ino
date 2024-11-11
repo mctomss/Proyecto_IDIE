22c88944084f54d1de6925db996114e4112ad1d9
JUANITO264 <juan.rruiz@mail.escuelaing.edu.co>
2024-11-11 15:50:41 -0500

    Fullt working HTTP Form

--- Código/Firmware/Firmware.ino
+++ Código/Firmware/Firmware.ino
@@ -1,8 +1,10 @@
+#define THINGER_SERIAL_DEBUG
 #include <Arduino.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncTCP.h>
 #include "LittleFS.h"
+#include <ThingerESP32.h>
 
 // Crear un servidor web en el puerto 80
 AsyncWebServer server(80);
@@ -11,15 +13,17 @@ AsyncWebServer server(80);
 const char* PARAM_INPUT_1 = "ssid";
 const char* PARAM_INPUT_2 = "pass";
 const char* PARAM_INPUT_3 = "muser";
-const char* PARAM_INPUT_4 = "mpass";
-const char* PARAM_INPUT_5 = "ip";
-const char* PARAM_INPUT_6 = "gateway";
+const char* PARAM_INPUT_4 = "device_id";
+const char* PARAM_INPUT_5 = "device_key";
+const char* PARAM_INPUT_6 = "ip";
+const char* PARAM_INPUT_7 = "gateway";
 
 // Variables para almacenar los valores de los campos del formulario
 String ssid;
 String pass;
 String muser;
-String mpass;
+String device_id;
+String device_key;
 String ip;
 String gateway;
 
@@ -27,9 +31,13 @@ String gateway;
 const char* ssidPath = "/ssid.txt";
 const char* passPath = "/pass.txt";
 const char* muserPath = "/muser.txt";
-const char* mpassPath = "/mpass.txt";
+const char* device_idPath = "/device_id.txt";
+const char* device_keyPath = "/device_key.txt";
 const char* ipPath = "/ip.txt";
 const char* gatewayPath = "/gateway.txt";
+
+// Definir la clase ThingerESP32
+//ThingerESP32* thing;
 
 //Configuración de la red
 IPAddress localIP;
@@ -122,16 +130,28 @@ void setup(){
     // Lectura de los valores almacenados en los archivos
     ssid = readFile(LittleFS, ssidPath);
     pass = readFile(LittleFS, passPath);
+    muser = readFile(LittleFS, muserPath);
+    device_id = readFile(LittleFS, device_idPath);
+    device_key = readFile(LittleFS, device_keyPath);
     ip = readFile(LittleFS, ipPath);
     gateway = readFile (LittleFS, gatewayPath);
     Serial.println(ssid);
     Serial.println(pass);
+    Serial.println(muser);
+    Serial.println(device_id);
+    Serial.println(device_key);
     Serial.println(ip);
     Serial.println(gateway);
     //
     if(initWiFi()) {
-    // Código para ejecutar si la conexión a la red WiFi es exitosa
-    Serial.println("Connected to WiFi");
+        // Código para ejecutar si la conexión a la red WiFi es exitosa
+        /*thing = new ThingerESP32(muser.c_str(), device_id.c_str(), device_key.c_str()); // Inicializar la clase ThingerESP32
+        pinMode(16, OUTPUT);
+        // digital pin control example (i.e. turning on/off a light, a relay, configuring a parameter, etc)
+        (*thing)["GPIO_16"] << digitalPin(16);
+
+        // resource output example (i.e. reading a sensor value)
+        (*thing)["millis"] >> outputValue(millis());*/
     }
     else {
         // Configurar el ESP32 como un punto de acceso
@@ -147,71 +167,80 @@ void setup(){
         server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
         request->send(LittleFS, "/wifimanager.html", "text/html");
         });
-        
+                
         server.serveStatic("/", LittleFS, "/");
         
         server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
-        int params = request->params();
-        for(int i=0;i<params;i++){
-            const AsyncWebParameter* p = request->getParam(i);
-            if(p->isPost()){
-            // HTTP Post valor de ssid
-            if (p->name() == PARAM_INPUT_1) {
-                ssid = p->value().c_str();
-                Serial.print("SSID set to: ");
-                Serial.println(ssid);
-                // Write file to save value
-                writeFile(LittleFS, ssidPath, ssid.c_str());
-            }
-            // HTTP POST valor de contraseña
-            if (p->name() == PARAM_INPUT_2) {
-                pass = p->value().c_str();
-                Serial.print("Password set to: ");
-                Serial.println(pass);
-                // Write file to save value
-                writeFile(LittleFS, passPath, pass.c_str());
-            }
-            // HTTP POST valor de usuario MQTT
-            if (p->name() == PARAM_INPUT_3) {
-                muser = p->value().c_str();
-                Serial.print("MQTT User set to: ");
-                Serial.println(muser);
-                // Write file to save value
-                writeFile(LittleFS, muserPath, muser.c_str());
-            }
-            // HTTP POST valor de contraseña MQTT
-            if (p->name() == PARAM_INPUT_4) {
-                mpass = p->value().c_str();
-                Serial.print("MQTT Password set to: ");
-                Serial.println(mpass);
-                // Write file to save value
-                writeFile(LittleFS, mpassPath, mpass.c_str());
-            }
-            // HTTP POST ip value
-            if (p->name() == PARAM_INPUT_5) {
-                ip = p->value().c_str();
-                Serial.print("IP Address set to: ");
-                Serial.println(ip);
-                // Write file to save value
-                writeFile(LittleFS, ipPath, ip.c_str());
-            }
-            // HTTP POST gateway value
-            if (p->name() == PARAM_INPUT_6) {
-                gateway = p->value().c_str();
-                Serial.print("Gateway set to: ");
-                Serial.println(gateway);
-                // Write file to save value
-                writeFile(LittleFS, gatewayPath, gateway.c_str());
-            }
-            }
-        }
-        request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
-        delay(3000);
-        ESP.restart();
+          int params = request->params();
+          for(int i=0;i<params;i++){
+              const AsyncWebParameter* p = request->getParam(i);
+              if(p->isPost()){
+              // HTTP Post valor de ssid
+              if (p->name() == PARAM_INPUT_1) {
+                  ssid = p->value().c_str();
+                  Serial.print("SSID set to: ");
+                  Serial.println(ssid);
+                  // Write file to save value
+                  writeFile(LittleFS, ssidPath, ssid.c_str());
+              }
+              // HTTP POST valor de contraseña
+              if (p->name() == PARAM_INPUT_2) {
+                  pass = p->value().c_str();
+                  Serial.print("Password set to: ");
+                  Serial.println(pass);
+                  // Write file to save value
+                  writeFile(LittleFS, passPath, pass.c_str());
+              }
+              // HTTP POST valor de usuario MQTT
+              if (p->name() == PARAM_INPUT_3) {
+                  muser = p->value().c_str();
+                  Serial.print("MQTT User set to: ");
+                  Serial.println(muser);
+                  // Write file to save value
+                  writeFile(LittleFS, muserPath, muser.c_str());
+              }
+              // HTTP POST valor de device_id
+              if (p->name() == PARAM_INPUT_4) {
+                  device_id = p->value().c_str();
+                  Serial.print("Device ID set to: ");
+                  Serial.println(device_id);
+                  // Write file to save value
+                  writeFile(LittleFS, device_idPath, device_id.c_str());
+              }
+              // HTTP POST valor de device_key
+              if (p->name() == PARAM_INPUT_5) {
+                  device_key = p->value().c_str();
+                  Serial.print("Device Credentials set to: ");
+                  Serial.println(device_key);
+                  // Write file to save value
+                  writeFile(LittleFS, device_keyPath, device_key.c_str());
+              }
+              // HTTP POST ip value
+              if (p->name() == PARAM_INPUT_6) {
+                  ip = p->value().c_str();
+                  Serial.print("IP Address set to: ");
+                  Serial.println(ip);
+                  // Write file to save value
+                  writeFile(LittleFS, ipPath, ip.c_str());
+              }
+              // HTTP POST gateway value
+              if (p->name() == PARAM_INPUT_7) {
+                  gateway = p->value().c_str();
+                  Serial.print("Gateway set to: ");
+                  Serial.println(gateway);
+                  // Write file to save value
+                  writeFile(LittleFS, gatewayPath, gateway.c_str());
+              }
+              }
+          }
+          request->send(200, "text/plain", "Done. ESP will restart, connect to your router and go to IP address: " + ip);
+          delay(3000);
+          ESP.restart();
         });
         server.begin();
     }
 }
 void loop(){
     // Código para ejecutar en el loop
+    //thing->handle();
 }
